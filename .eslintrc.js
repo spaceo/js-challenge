faaa7490471ec430385e99f0e7b392b8afe9eecf
Mikkel Jakobsen <mikkel@adapt.dk>
2018-11-25 13:38:31 +0100

    Adding some infrastructure. No Readme

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'browser': true,
+        'mocha': true,
+        'commonjs': true,
+    },
+    'extends': 'eslint:recommended',
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-var': [
+            'error',
+        ]
+    }
+};
